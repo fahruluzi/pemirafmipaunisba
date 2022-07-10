ca25395c6983bd549fd930896af726323cb0101a
Fahrul Fauzi <fahrul.fauz@gmail.com>
2022-07-10 10:39:44 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// next.config.js
+module.exports = {
+  // ... rest of the configuration.
+  output: 'standalone',
 }
-
-module.exports = nextConfig
